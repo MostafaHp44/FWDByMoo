51dd3a1b522d3a8fe57332572adc9df0f5546250
MoustafaHp <mostafahp44@gmail.com>
2022-12-12 15:07:55 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
-const section=document.getElementsByName('section')
-const ul=document.getElementsByClassName('NavBarItem')
-let emp =[]
-
-const Navfunc=()=>{
-    for(let sec of section)
-{
-    let li=sec.attributes['data-nav'].nodeValue;
-    console.log(li)    
-    ul.innerHTML+=`<li><a href="" class="nav_item">${li}</a></li>`;
-    emp.push(sec.offsetTop)
-
-}
-
-}
-Navfunc();
+/**/
